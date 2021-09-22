7b208d35d0295393f5b30ad8abf574290454a01a
Eddy Hernandez <eddyhdzg@gmail.com>
2021-09-21 21:52:32 -0500

    :construction_worker: github dev CI

--- src/fb/firebaseConfig.ts
+++ src/fb/firebaseConfig.ts
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCsg8RnedJzLzNTNKlytNQcBjOO4R63OCg",
-  authDomain: "solarx-firebase.firebaseapp.com",
-  projectId: "solarx-firebase",
-  storageBucket: "solarx-firebase.appspot.com",
-  messagingSenderId: "404633824290",
-  appId: "1:404633824290:web:1c3995fd119c13e8242e7b",
-  measurementId: "G-ZP9YX74YJ4",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export default firebaseConfig;
--- src/pages/wallet/walletHome/WalletHome.tsx
+++ src/pages/wallet/walletHome/WalletHome.tsx
@@ -23,9 +23,10 @@ export default function WalletHome() {
         description={t("pages.wallet.description")}
       />
       <PageTitle>
-        {t("pages.wallet.title", {
+        {/* {t("pages.wallet.title", {
           postProcess: "capitalize",
-        })}
+        })} */}
+        {String(process.env?.REACT_APP_ENV)}
       </PageTitle>
       {lg ? <WalletDesktopLayout /> : <WalletMobileLayout />}
     </>
